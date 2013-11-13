66d51708f49327f05f9ada67fc05d3c8ac36051d
Graham Ollis <perl@wdlabs.com>
2013-11-13 11:22:58 -0500

    generate constants from XS version

---
+++ inc/prep.pl
@@ -0,0 +1,38 @@
+use strict;
+use warnings;
+use Archive::Libarchive::XS;
+use Path::Class qw( file dir );
+
+do { # constants.pm
+
+  my $file = file(__FILE__)->parent->parent->file(qw( lib Archive Libarchive FFI constants.pm ));
+  
+  $file->parent->mkpath(0,0755);
+  
+  my $fh = $file->openw;
+  
+  print $fh "package Archive::Libarchive::FFI;\n\n";
+  print $fh "use strict;\n";
+  print $fh "use warnings;\n\n";
+  
+  print $fh "use constant {\n";
+  foreach my $const (sort @{ $Archive::Libarchive::XS::EXPORT_TAGS{const} })
+  {
+    my $value = eval qq{ Archive::Libarchive::XS::$const() };
+    print $fh "  $const => $value,\n";
+  }
+  print $fh "};\n\n";
+  
+  print $fh "push \@{ \$Archive::Libarchive::FFI::EXPORT_TAGS{const} }, qw(\n";
+  
+  foreach my $const (sort @{ $Archive::Libarchive::XS::EXPORT_TAGS{const} })
+  {
+    print $fh "  $const\n";
+  }
+  
+  print $fh ")\n\n";
+  
+  print $fh "1;\n";
+  
+  close $fh;
+};
--- lib/Archive/Libarchive/FFI.pm
+++ lib/Archive/Libarchive/FFI.pm
@@ -9,48 +9,26 @@ use base qw( Exporter );
 # ABSTRACT: Perl bindings to libarchive via FFI
 # VERSION
 
-our @EXPORT_OK = qw(
+our %EXPORT_TAGS = ( all => [], const => [], func => [] );
+
+require Archive::Libarchive::FFI::constants;
+
+@{ $EXPORT_TAGS{func} } = qw(
   archive_read_new
   archive_read_support_filter_all
   archive_read_support_format_all
   archive_read_open_filename
   archive_read_free
-  archive_error_string
-  
+  archive_error_string  
   archive_read_next_header
   archive_entry_pathname
   archive_read_data_skip
-  
-  ARCHIVE_OK
 );
 
-our %EXPORT_TAGS = ( all => \@EXPORT_OK );
+our @EXPORT_OK = (map { @{ $EXPORT_TAGS{$_} } } qw( const func ));
+$EXPORT_TAGS{all} = \@EXPORT_OK;
 
 ffi_lib \$_ for map { print "$_\n" if 0; $_ } DynaLoader::dl_findfile(split /\s+/, Alien::Libarchive->new->libs);
-
-use constant {
-  ARCHIVE_EOF    => 1,
-  ARCHIVE_OK     => 0,
-  ARCHIVE_RETRY  => -10,
-  ARCHIVE_WARN   => -20,
-  ARCHIVE_FAILED => -25,
-  ARCHIVE_FATAL  => -30,
-  
-  ARCHIVE_FILTER_NONE     => 0,
-  ARCHIVE_FILTER_GZIP     => 1,
-  ARCHIVE_FILTER_BZIP2    => 2,
-  ARCHIVE_FILTER_COMPRESS => 3,
-  ARCHIVE_FILTER_PROGRAM  => 4,
-  ARCHIVE_FILTER_LZMA     => 5,
-  ARCHIVE_FILTER_XZ       => 6,
-  ARCHIVE_FILTER_UU       => 7,
-  ARCHIVE_FILTER_RPM      => 8,
-  ARCHIVE_FILTER_LZIP     => 9,
-  ARCHIVE_FILTER_LRZIP    => 10,
-  ARCHIVE_FILTER_LZOP     => 11,
-  ARCHIVE_FILTER_GRZIP    => 12,
-
-};
 
 attach_function 'archive_read_new', undef, _ptr;
 attach_function 'archive_read_support_filter_all', [ _ptr ], _int;
