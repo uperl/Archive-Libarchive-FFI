7247f93b5295ab496e1e3f31244b922946a0c466
Graham Ollis <perl@wdlabs.com>
2013-11-13 12:38:41 -0500

    build up export list as we attach

--- lib/Archive/Libarchive/FFI.pm
+++ lib/Archive/Libarchive/FFI.pm
@@ -4,33 +4,25 @@ use strict;
 use warnings;
 use Alien::Libarchive;
 use FFI::Raw::PtrPtr;
-use FFI::Sweet qw( ffi_lib attach_function :types );
+use FFI::Sweet qw( ffi_lib :types );
 use base qw( Exporter );
 
 # ABSTRACT: Perl bindings to libarchive via FFI
 # VERSION
+
+ffi_lib \$_ for map { print "$_\n" if 0; $_ } DynaLoader::dl_findfile(split /\s+/, Alien::Libarchive->new->libs);
 
 our %EXPORT_TAGS = ( all => [], const => [], func => [] );
 
 require Archive::Libarchive::FFI::constants;
 require Archive::Libarchive::FFI::functions;
 
-@{ $EXPORT_TAGS{func} } = qw(
-  archive_read_new
-  archive_read_support_filter_all
-  archive_read_support_format_all
-  archive_read_open_filename
-  archive_read_free
-  archive_error_string  
-  archive_read_next_header
-  archive_entry_pathname
-  archive_read_data_skip
-);
-
-our @EXPORT_OK = (map { @{ $EXPORT_TAGS{$_} } } qw( const func ));
-$EXPORT_TAGS{all} = \@EXPORT_OK;
-
-ffi_lib \$_ for map { print "$_\n" if 0; $_ } DynaLoader::dl_findfile(split /\s+/, Alien::Libarchive->new->libs);
+sub attach_function ($$$)
+{
+  my ( $name, $arg, $rv ) = @_;
+  push @{ $EXPORT_TAGS{func} }, $name;
+  FFI::Sweet::attach_function($name, $arg, $rv);
+}
 
 attach_function 'archive_read_new', undef, _ptr;
 attach_function 'archive_read_support_filter_all', [ _ptr ], _int;
@@ -41,6 +33,8 @@ attach_function 'archive_error_string', [ _ptr ], _str;
 attach_function 'archive_entry_pathname', [ _ptr ], _str;
 attach_function 'archive_read_data_skip', [ _ptr ], _int;
 
+push @{ $EXPORT_TAGS{func} }, qw( archive_read_next_header );
+
 sub archive_read_next_header
 {
   my $entry = FFI::Raw::PtrPtr->new;  
@@ -48,5 +42,8 @@ sub archive_read_next_header
   $_[1] = $entry->dereference;
   $ret;
 }
+
+our @EXPORT_OK = (map { @{ $EXPORT_TAGS{$_} } } qw( const func ));
+$EXPORT_TAGS{all} = \@EXPORT_OK;
 
 1;
